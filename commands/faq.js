a0429f60bd624c0dc4ecec541dfb043c288dc1e4
vFlyingCowboy <103060762+vFlyingCowboy@users.noreply.github.com>
2022-07-01 20:12:13 -0700

    changed spacing on FAQ command

--- commands/faq.js
+++ commands/faq.js
@@ -5,6 +5,6 @@ data: new SlashCommandBuilder()
     .setname('faq')
     .setDescription('Gives members of VATUSA a link to our FAQ page!'),
     async execute(interaction) {
-		await interaction.reply({content: "Have you checked the VATUSA FAQ for the answer to your question? https://www.vatusa.net/help/kb", ephemeral: true});
-	},
+        await interaction.reply({content: "Have you checked the VATUSA FAQ for the answer to your question? https://www.vatusa.net/help/kb", ephemeral: true});
+    },
 };
