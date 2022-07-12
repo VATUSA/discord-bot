6a288c752937d96115995fcc00424ec95d5b3bbc
Jon <jonhasacat8@gmail.com>
2022-07-12 11:52:26 -0700

    Fix typo

--- commands/faq.js
+++ commands/faq.js
@@ -2,7 +2,7 @@ const {SlashCommandBuilder} = require('@discordjs/builders')
 
 module.exports = {
 data: new SlashCommandBuilder()
-    .setname('faq')
+    .setName('faq')
     .setDescription('Gives members of VATUSA a link to our FAQ page!'),
     async execute(interaction) {
         await interaction.reply({content: "Have you checked the VATUSA FAQ for the answer to your question? https://www.vatusa.net/help/kb"});
