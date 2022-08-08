2fa8fbc008a0fa500db2391339c94f68674f8666
vFlyingCowboy <103060762+vFlyingCowboy@users.noreply.github.com>
2022-08-07 22:15:07 -0700

    fix double code

--- commands/faq.js
+++ commands/faq.js
@@ -1,15 +1,9 @@
 const {SlashCommandBuilder} = require('@discordjs/builders')
 
 module.exports = {
-<<<<<<< HEAD
     data: new SlashCommandBuilder()
-        .setname('faq')
+        .setName('faq')
         .setDescription('Gives members of VATUSA a link to our FAQ page!'),
-=======
-data: new SlashCommandBuilder()
-    .setName('faq')
-    .setDescription('Gives members of VATUSA a link to our FAQ page!'),
->>>>>>> 6a288c752937d96115995fcc00424ec95d5b3bbc
     async execute(interaction) {
         await interaction.reply({content: "Have you checked the VATUSA FAQ for the answer to your question? https://www.vatusa.net/help/kb"});
     },
